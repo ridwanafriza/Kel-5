9a03dbe1085a577474a10f8ec454ce3477bd2d6f
starinatiara <pramestistarina@gmail.com>
2025-12-12 15:07:53 +0700

    detailkerajinan

---
+++ app/src/main/java/com/id/myresep/Starina/DetailKerajinanActivity.kt
@@ -0,0 +1,21 @@
+package com.id.myresep.Starina
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.id.myresep.R
+
+class DetailKerajinanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_kerajinan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
