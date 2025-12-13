59e5f97559718abdd3169b151a24ae6af89ceb63
ridwan <ralistiyono09@gmail.com>
2025-12-13 10:07:48 +0700

    membuat navigasi, sudah bisa dipakai -ridwan

--- app/src/main/java/com/id/myresep/MainActivity.kt
+++ app/src/main/java/com/id/myresep/MainActivity.kt
@@ -19,8 +19,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-
-
         }
 
         val navHostFragment =
